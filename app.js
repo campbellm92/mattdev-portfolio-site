6dda78eda985646968c2a012bf84e65c39981795
Matthew Campbell <m.campbell92@gmail.com>
2024-08-02 10:45:14 +0200

    remove comments

--- app.js
+++ app.js
@@ -27,49 +27,6 @@ app.use("/stack", stackRouter);
 app.use("/now", nowRouter);
 app.use("/contact", contactRouter);
 
-// app.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
-
-// app.get("/posts", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "posts.html"));
-// });
-
-// app.get("/posts-list", (req, res) => {
-//   const postsDir = path.join(__dirname, "posts");
-//   fs.readdir(postsDir, (error, files) => {
-//     if (error) {
-//       return res.status(500).json({ error: "Unable to read posts directory." });
-//     }
-//     const posts = files.map((file) => {
-//       const filePath = path.join(postsDir, file);
-//       const content = fs.readFileSync(filePath, "utf-8");
-//       const title = content.split("\n")[0].replace(/^#\s*/, "");
-//       return { file, title };
-//     });
-//     res.json(posts);
-//   });
-// });
-
-// app.get("/post", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "templates", "post.html"));
-// });
-
-// app.get("/posts/:file", (req, res) => {
-//   const filePath = path.join(__dirname, "posts", req.params.file);
-//   fs.readFile(filePath, "utf-8", (error, content) => {
-//     if (error) {
-//       return res.status(404).send("Post not found.");
-//     }
-//     const htmlContent = marked(content);
-//     res.send(htmlContent);
-//   });
-// });
-
-// app.get("/projects", (req, res) => {
-//   res.sendFile(path.join(__dirname, "public", "projects.html"));
-// });
-
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
