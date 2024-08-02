f3be3467f0c804ff9daaf3fddda914302bc81849
Matthew Campbell <m.campbell92@gmail.com>
2024-08-02 10:47:59 +0200

    experiment w/ async funcs for loading posts, bug fix

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const router = express.Router();
 
@@ -7,20 +7,22 @@ router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public", "posts.html"));
 });
 
-router.get("/list", (req, res) => {
-  const postsDir = path.join(__dirname, "../posts");
-  fs.readdir(postsDir, (error, files) => {
-    if (error) {
-      return res.status(500).json({ error: "Unable to read posts directory." });
-    }
-    const posts = files.map((file) => {
-      const filePath = path.join(postsDir, file);
-      const content = fs.readFileSync(filePath, "utf-8");
-      const title = content.split("\n")[0].replace(/^#\s*/, "");
-      return { file, title };
-    });
+router.get("/list", async (req, res) => {
+  try {
+    const postsDir = path.join(__dirname, "../posts");
+    const files = await fs.readdir(postsDir);
+    const posts = await Promise.all(
+      files.map(async (file) => {
+        const filePath = path.join(postsDir, file);
+        const content = await fs.readFile(filePath, "utf-8");
+        const title = content.split("\n")[0].replace(/^#\s*/, "");
+        return { file, title };
+      })
+    );
     res.json(posts);
-  });
+  } catch (error) {
+    res.status(500).json({ error: "Unable to retrieve posts." });
+  }
 });
 
 module.exports = router;
